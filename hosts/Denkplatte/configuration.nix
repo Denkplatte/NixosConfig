50c0d8dcd8f6cccdd3fdc3ca5ad9a1290fff8384
Denkplatte <player123576@web.de>
2026-03-28 18:00:38 +0100

    new locale.nix

--- hosts/Denkplatte/configuration.nix
+++ hosts/Denkplatte/configuration.nix
@@ -10,6 +10,7 @@
       ./hardware-configuration.nix
       ../../modules/system/boot.nix
       ../../modules/system/nvidia.nix
+      ../../modules/system/locale.nix
       #./modules
   
     ];
@@ -26,24 +27,7 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
-  time.timeZone = "Europe/Berlin";
-
-  # Select internationalisation properties.
-  #i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "de_DE.UTF-8";
-    LC_IDENTIFICATION = "de_DE.UTF-8";
-    LC_MEASUREMENT = "de_DE.UTF-8";
-    LC_MONETARY = "de_DE.UTF-8";
-    LC_NAME = "de_DE.UTF-8";
-    LC_NUMERIC = "de_DE.UTF-8";
-    LC_PAPER = "de_DE.UTF-8";
-    LC_TELEPHONE = "de_DE.UTF-8";
-    LC_TIME = "de_DE.UTF-8";
-  };
-
+ 
 
   # Enable the X11 windowing system.
   #services.xserver.enable = true;
