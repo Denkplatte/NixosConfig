6601de94b019fcfabfdccdeb5ea4540d5d27c9ee
Denkplatte <player123576@web.de>
2026-03-29 21:11:42 +0200

    updated to same versions

--- flake.nix
+++ flake.nix
@@ -2,10 +2,13 @@
   description = "Clean NixOS config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/25.11";
-    home-manager.url = "github:nix-community/25.11";
-  };
+  nixpkgs.url = "github:NixOS/nixpkgs/25.11";
 
+  home-manager = {
+    url = "github:nix-community/home-manager/release-25.11";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+};
   outputs = { self, nixpkgs, home-manager, ... }:
   let
     system = "x86_64-linux";
