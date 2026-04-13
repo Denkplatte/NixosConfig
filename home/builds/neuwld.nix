daae648a1b37813370ae8f501c39f1c3ebfac744
Denkplatte <player123576@web.de>
2026-04-13 21:46:02 +0200

    fix neuwld deps and makefile vars

--- home/builds/neuwld.nix
+++ home/builds/neuwld.nix
@@ -1,14 +1,25 @@
-{ stdenv, fetchzip, bmake, pkg-config, libdrm, lib }:
+{ stdenv, fetchzip, bmake, pkg-config, libdrm, freetype, fontconfig, pixman, lib }:
 
 stdenv.mkDerivation {
   pname = "neuwld";
   version = "unstable";
   src = fetchzip {
-    url = "https://git.sr.ht/~dlm/neuwld/archive/6446a281.tar.gz";
-    hash = lib.fakeHash;
+    url = "https://git.sr.ht/~shrub900/neuwld/archive/6446a281.tar.gz";
+    hash = "sha256-rP03qodS9zUKJ6WPxPlu/sn+yRWc6jssa10mVPEjodc=";
   };
   nativeBuildInputs = [ bmake pkg-config ];
-  buildInputs = [ libdrm ];
-  buildPhase = "bmake PREFIX=$out";
-  installPhase = "bmake PREFIX=$out install";
+  buildInputs = [ libdrm freetype fontconfig pixman ];
+  buildPhase = ''
+    bmake PREFIX=$out \
+      ENABLE_DEBUG=0 \
+      ENABLE_STATIC=0 \
+      ENABLE_SHARED=1
+  '';
+  installPhase = ''
+    bmake PREFIX=$out \
+      ENABLE_DEBUG=0 \
+      ENABLE_STATIC=0 \
+      ENABLE_SHARED=1 \
+      install
+  '';
 }
