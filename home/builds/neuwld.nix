bc5335eb7c128c118d53ef6270ae1092f79ee103
Denkplatte <player123576@web.de>
2026-04-13 22:17:40 +0200

    fixed neuwld.nix

--- home/builds/neuwld.nix
+++ home/builds/neuwld.nix
@@ -1,25 +1,12 @@
-{ stdenv, fetchzip, bmake, pkg-config, libdrm, freetype, fontconfig, pixman, lib }:
+{ stdenv, fetchzip, meson, ninja, pkg-config, libdrm, freetype, fontconfig, pixman, wayland, lib }:
 
 stdenv.mkDerivation {
   pname = "neuwld";
   version = "unstable";
   src = fetchzip {
-    url = "https://git.sr.ht/~shrub900/neuwld/archive/6446a281.tar.gz";
+    url = "https://git.sr.ht/~shrub900/neuwld/archive/HEAD.tar.gz";
     hash = "sha256-rP03qodS9zUKJ6WPxPlu/sn+yRWc6jssa10mVPEjodc=";
   };
-  nativeBuildInputs = [ bmake pkg-config ];
-  buildInputs = [ libdrm freetype fontconfig pixman ];
-  buildPhase = ''
-    bmake PREFIX=$out \
-      ENABLE_DEBUG=0 \
-      ENABLE_STATIC=0 \
-      ENABLE_SHARED=1
-  '';
-  installPhase = ''
-    bmake PREFIX=$out \
-      ENABLE_DEBUG=0 \
-      ENABLE_STATIC=0 \
-      ENABLE_SHARED=1 \
-      install
-  '';
+  nativeBuildInputs = [ meson ninja pkg-config ];
+  buildInputs = [ libdrm freetype fontconfig pixman wayland];
 }
